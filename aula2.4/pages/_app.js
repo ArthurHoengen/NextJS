802a390cdd1c930f5384d4720d1f2463fff7a931
ArthurHoengen <arthurhoengen@gmail.com>
2024-03-15 10:30:40 -0300

    Finalizando aula 2.4

---
+++ aula2.4/pages/_app.js
@@ -0,0 +1,9 @@
+import {Provider} from '@skynexui/components'
+
+export default function MyApp({Component, pageProps}) {
+  return (
+    <Provider>
+      <Component {...pageProps}/>
+    </Provider>
+  )
+}
